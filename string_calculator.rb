1cd89912ab6f9aa4899199044a30c4f1ac71c074
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-07 01:14:50 +0530

    Adds logic to support different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,21 @@
 class StringCalculator
   # define class method add to implement the string calculator logic which takes an string input
   def self.add(numbers)
-    computed_numbers = numbers.split(",").map { |number| number.include?("\n") ? number.split("\n") : number }
+    # Default delimiter is set to comma
+    delimiter = ','
+
+    # If the input string starts with "//", it indicates a custom delimiter is specified
+    if numbers.start_with?("//")
+      # Extract the custom delimiter from the input string
+      delimiter = numbers[2]
+
+      # Remove the delimiter declaration from the input string
+      numbers = numbers[4..-1]
+    end
+
+    # Split the input string into an array of numbers based on the delimiter or newline character
+    computed_numbers = numbers.split("#{delimiter}").map { |number| number.include?("\n") ? number.split("\n") : number }
+    # Convert each element of the array to an integer and sum them up
     computed_numbers.flatten.map(&:to_i).sum
   end
 end
