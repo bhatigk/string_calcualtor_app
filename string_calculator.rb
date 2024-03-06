7e0dfaea6f16d8f13001b3273fe3849e7aacdb57
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-06 23:58:31 +0530

    Defines add method to implement logic

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  # define class method add to implement the string calculator logic which takes an string input
+  def self.add(numbers)
+  end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,5 +1,11 @@
+# Require RSpec, It is a testing tool for Ruby, created for behavior-driven development (BDD)
 require 'rspec'
+# Require the StringCalculator class
 require_relative 'string_calculator'
 
+# Describe the behavior of the StringCalculator class
 RSpec.describe StringCalculator do
+  # Describe the add method
+  describe ".add" do
+  end
 end
