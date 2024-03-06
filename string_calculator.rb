df40a46c1cf46276559c7efba7fe57cb7835d451
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-07 01:09:21 +0530

    Adds logic to handles new lines between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   # define class method add to implement the string calculator logic which takes an string input
   def self.add(numbers)
-    numbers.split(",").map(&:to_i).sum
+    computed_numbers = numbers.split(",").map { |number| number.include?("\n") ? number.split("\n") : number }
+    computed_numbers.flatten.map(&:to_i).sum
   end
 end
