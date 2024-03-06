288f45c3bb01d3d50e9154c6ca540797fd4d6f3a
Gopalkrishna <gopalkrishnabhati1@gmail.com>
2024-03-07 00:48:41 +0530

    Refactors code

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,6 @@
 class StringCalculator
   # define class method add to implement the string calculator logic which takes an string input
   def self.add(numbers)
-    return 0 if numbers.empty?
-
     numbers.split(",").map(&:to_i).sum
   end
 end
